ab7db29dd2ec29b58bc33a4b9b83ac1f12b2e3ac
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-25 01:04:54 +0530

    frontend

--- main.tf
+++ main.tf
@@ -1,40 +1,39 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = merge(var.tags, {Name = "${var.env}-vpc"})
+  tags                 = merge(var.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
   source = "./subnets"
 
-  for_each = var.subnets
-  vpc_id = aws_vpc.main.id
+  for_each   = var.subnets
+  vpc_id     = aws_vpc.main.id
   cidr_block = each.value["cidr_block"]
-  name = each.value["name"]
-  azs = each.value["azs"]
-  tags = var.tags
-  env = var.env
-}
+  name       = each.value["name"]
+  azs        = each.value["azs"]
 
+  tags = var.tags
+  env  = var.env
+}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = tags = merge(var.tags, {Name = "${var.env}-igw"})
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
 }
 
 resource "aws_eip" "ngw" {
-      count = length(var.subnets["public"].cidr_block)
-      vpc      = true
-      tags = tags = merge(var.tags, {Name = "${var.env}-ngw"})
+  count = length(var.subnets["public"].cidr_block)
+  vpc   = true
+  tags  = merge(var.tags, { Name = "${var.env}-ngw" })
 }
 
 resource "aws_nat_gateway" "ngw" {
-    count = length(var.subnets["public"].cidr_block)
-    allocation_id = aws_eip.ngw[count.index].id
-    subnet_id     = module.subnets["public"].subnet_ids[count.index]
+  count         = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
 
-tags = tags = merge(var.tags, {Name = "${var.env}-ngw"})
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
 }
-
